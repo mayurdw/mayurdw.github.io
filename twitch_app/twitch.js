ce5765897b3a5af207a6a8327541853b26b84c4c
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2017-11-20 22:43:52 +1300

    Update to provide dummy data

--- twitch_app/twitch.js
+++ twitch_app/twitch.js
@@ -1,41 +1,54 @@
+var szClient_id;
 
-$(document).ready(function() {
+$( document ).ready( function() {
     getStatus();
 })
 
-function getStatus(params) {
-    var szClient_id = 'f8zbrjgdn03q5ylbjicjoktfxilvqo';
-    var szTwitchEndpoint = 'https://api.twitch.tv/kraken/';
-    var szFCC = 'freecodecamp';
-    var szVersionHeader = 'application/vnd.twitchtv.v5+json';
-    var szFCCUrl = szTwitchEndpoint + 'users?login=' + szFCC;
-    var obMyHeaders = new Headers();
-    obMyHeaders.append('Client-ID', szClient_id);
-    obMyHeaders.append('Accept', szVersionHeader);
-    var obMyGet = { 
-        method: 'GET',
-        headers: obMyHeaders,
-        mode: 'cors',
-        cache: 'default' 
-    };
-    
-    fetch(szFCCUrl, obMyGet)
-        .then(response => response.json())
-        .then(function(JSONResponse) {
+function getStatus( ) {
+
+    if ( szClient_id == null )
+    {
+        $( '#FCCStreaming' ).html( "STREAMING!" ).css( { 'color':'green' } );
+    }
+    else{
+        var szTwitchEndpoint = 'https://api.twitch.tv/kraken/';
+        var szFCC = 'freecodecamp';
+        var szVersionHeader = 'application/vnd.twitchtv.v5+json';
+        var szFCCUrl = szTwitchEndpoint + 'users?login=' + szFCC;
+        var obMyHeaders = new Headers();
+        obMyHeaders.append( 'Client-ID', szClient_id );
+        obMyHeaders.append( 'Accept', szVersionHeader );
+        var obMyGet = { 
+            method: 'GET',
+            headers: obMyHeaders,
+            mode: 'cors',
+            cache: 'default' 
+        };
+        
+        //first call for FCC status
+        fetch( szFCCUrl, obMyGet )
+        .then( response => response.json() )
+        .then( function( JSONResponse ) {
             szFCC = JSONResponse.users[0]._id;
             var szStreamUrl = szTwitchEndpoint + 'streams/' +   szFCC + '?client_id=' + szClient_id;
-            fetch(szStreamUrl)
-            .then(response => response.json())
-            .then(function(stream){   
-                //console.log( stream.stream );
+            fetch( szStreamUrl )
+            .then( response => response.json() )
+            .then( function( stream ){
                 if ( stream.stream != null )
                 {
-                    $('#FCCStreaming').html("STREAMING!").css({'color':'green'});
+                    $( '#FCCStreaming' ).html( "STREAMING!" ).css( { 'color':'green' } );
                 }
                 else
                 {
-                    $('#FCCStreaming').html("OFFLINE!").css({'color':'red'});
+                    $( '#FCCStreaming' ).html( "OFFLINE!" ).css( { 'color':'red' } );
                 }
             })
+            .catch(function( err ){
+                console.log( err );
+            })
         })
+        .catch(function(err){
+            console.log( err );
+        })
+    }
 };
